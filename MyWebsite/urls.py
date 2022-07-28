b2e03cc0cde3d21f3b35ade05a33b2ec8dbd1c12
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-07-28 23:21:40 +0430

    blog home

--- MyWebsite/urls.py
+++ MyWebsite/urls.py
@@ -18,9 +18,11 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from website.urls import *
+from blog.urls import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls'))
+    path('', include('website.urls')),
+    path('blog' , include('blog.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from blog.views import *
+app_name='blog'
+urlpatterns =[
+    path('' , blog_view , name='index' )
+]
