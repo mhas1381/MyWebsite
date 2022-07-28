6aee1b8c8a925c686346d991dbd175b0728a19fe
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-07-28 16:38:21 +0430

    index page

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def index_view(request):
-    return HttpResponse("index Page")
+    return render(request,'website/index.html')
