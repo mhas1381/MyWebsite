20e390823589187ed20da22fa5829ef3c7ac8ee1
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-07-29 20:32:52 +0430

    register contact in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from blog.models import Post
+from website.models import Contact
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
@@ -8,5 +9,10 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title' , 'status' ,'published_date', 'created_date')
     list_filter = ('status' ,)
     search_fields = ['title' , 'content']
-
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date' 
+    list_display = ('name' , 'email' , 'created_date')
+    list_filter = ('email',)
+    search_fields = ('name' , 'messages')
 admin.site.register(Post , PostAdmin)
+admin.site.register(Contact , ContactAdmin)
