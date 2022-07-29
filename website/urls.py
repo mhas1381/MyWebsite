75551f6f2b520b1c39e29d54b59dbf1734e1ea1b
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-07-29 13:21:54 +0430

    fix base.html

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from website.views import *
 app_name = 'website'
 urlpatterns=[
-    path("",index_view , name='index')
+    path("",index_view , name='index'),
+    path('contact' , contact_view , name='contact')
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def index_view(request):
     return render(request,'website/index.html')
+def contact_view(request):
+    return render(request,'website/contact.html')
