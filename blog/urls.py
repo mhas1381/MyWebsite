6d45bb8a74a2d314e18b4939fb6a66706f86554b
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-08-03 17:35:41 +0430

    fix blog single view

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from blog.views import *
 app_name='blog'
 urlpatterns =[
-    path('' , blog_view , name='index' )
+    path('' , blog_view , name='index' ),
+    path('' , blog_single , name='single' )
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 def blog_view(request):
-    return render(request,'blog/blog.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html' , context)
+def blog_single(request):
+    return render(request,'blog/blog-single.html')
