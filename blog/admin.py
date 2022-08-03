df3dd43d324c3d486a25204878f2bb2e93c9e3d5
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-08-03 18:38:43 +0430

    adding author to blog

--- blog/admin.py
+++ blog/admin.py
@@ -6,8 +6,8 @@ from website.models import Contact
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy= 'created_date'
     empty_value_display = '-empty-'
-    list_display = ('title' , 'status' ,'published_date', 'created_date')
-    list_filter = ('status' ,)
+    list_display = ('title' ,'author', 'status' ,'published_date', 'created_date')
+    list_filter = ('status' ,'author')
     search_fields = ['title' , 'content']
 class ContactAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date' 
