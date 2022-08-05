8a7cc33a8e246f0c7e36c034fa84e0902df8ef2e
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2022-08-05 10:29:28 +0430

    post categories and latest posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,18 @@
 from django import template
+from blog.models import Post,Category
 
 register = template.Library()
+
+@register.inclusion_tag('blog/latest-posts.html')
+def latest_posts():
+    posts = Post.objects.filter(status=1).order_by('-published_date')[:4]
+    return {'posts':posts}
+
+@register.inclusion_tag('blog/post-categories.html')
+def post_categories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories':cat_dict}
