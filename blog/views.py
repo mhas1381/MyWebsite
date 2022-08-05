7d9253cae432d7478c1e807292908f52c2e23980
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2022-08-05 10:54:25 +0430

    fix categories

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,9 @@ def blog_single(request , pid):
     post = get_object_or_404(posts , pk=pid , status =1)
     context = {'post':post}
     return render(request,'blog/blog-single.html' , context)
+
+def blog_categories(request,cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html' , context)
