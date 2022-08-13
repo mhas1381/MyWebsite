1169d4cd9636f044c9870c51d8c773cec877d3c0
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2022-08-13 12:46:34 +0430

    login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from accounts.views import *
 app_name = 'accounts'
 urlpatterns=[
     path('login/' , login_view , name = 'login'),
+    path('logout' , logout_view , name ='logout'),
     path('signup/' , signup_view , name = 'signup'),
 ]
