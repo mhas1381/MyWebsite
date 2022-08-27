ece793ca612b7fd42c0a9ff5f8de94df5078c386
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-08-27 22:58:57 +0430

    get ready for production

--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -16,16 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!yt55dq)xfo0kso+8qcw_0*&amr$yln$%e^@_8i(kcjelrz*-e'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -48,15 +38,13 @@ INSTALLED_APPS = [
     'accounts'
 ]
 
-#site framework
-SITE_ID = 2
 
 #robots
 ROBOTS_USE_HOST = False
 ROBOTS_USE_SITEMAP = False
 
 # summernote
-X_FRAME_OPTIONS = "SAMEORIGIN"
+
 SUMMERNOTE_THEME = 'bs4'  
 SUMMERNOTE_CONFIG = {
     'iframe': True,
@@ -109,15 +97,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'MyWebsite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -157,14 +137,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
